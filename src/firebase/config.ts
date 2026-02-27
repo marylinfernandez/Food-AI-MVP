3ec2c4807c2db3bd996b87a4047fbbafa38170c8
Flor Marylin Fernandez Bautista <florfernandezbautista@gmail.com>
2026-02-27 12:13:16 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-5921933143-73826",
+  "appId": "1:844646721590:web:2c1b3344b7dd5faf34d097",
+  "apiKey": "AIzaSyAe9wKYS3jO1mmta5BxCTvoasiQAm25eS4",
+  "authDomain": "studio-5921933143-73826.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "844646721590"
+};
